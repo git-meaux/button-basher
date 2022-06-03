ebabb3e78e9d1410e41eec26f20096ce4b47cc8a
git-meaux <maurice.vd.erve@gmail.com>
2022-06-03 14:17:14 +0200

    feat:mutable audio

--- src/screens/Start.jsx
+++ src/screens/Start.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import "./Start.css";
 export default function StartScreen(){
+    const [muted,setMuted] =useState(false)
+
     return <div className="StartScreen">
         <div className="ClickHere">Click Here To Start</div>
-        {/* <iframe src=”https://olafwempe.com/mp3/silence/silence.mp3″ type=”audio/mp3″ allow=”autoplay” id=”audio” style=”display:none”></iframe> */}
-        <iframe src={require("./sound/soundtrack.webm")} type="audio/webm" allow="autoplay" id="audio" style={{display:"none"}} ></iframe>
-        {/* <audio autoPlay loop id="playAudio">
+        {/* <iframe src={require("./sound/soundtrack.webm")} type="audio/webm" allow="autoplay" id="audio" style={{display:"none"}} ></iframe> */}
+        <div className={ muted ?"mute":"notmute" } onClick={()=>setMuted(!muted) } ><img alt="" src={require("./images/muted.png")} /></div>
+        <audio autoPlay loop id="playAudio" muted={muted}>
             <source src={require("./sound/soundtrack.webm")} type="audio/webm" />
-        </audio> */}
+        </audio>
     </div>
 }
